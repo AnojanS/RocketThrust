993590f00535f842b369c932667a8802c8e83f01
AnojanS <asaba091@uottawa.ca>
2019-03-31 19:44:20 -0400

    started rocket thrust implementation

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,26 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Rigidbody rigidBody;
+
+    // Use this for initialization
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput(); 
+        ProcessInput();
     }
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        if (Input.GetKey(KeyCode.Space)) //thrust while rotating
         {
-            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))
